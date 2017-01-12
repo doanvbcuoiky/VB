517a0682630a4b493c59fea404b5c17766952458
1460202 <nguyenduy2496@outlook.com.vn>
2017-01-12 16:37:48 +0700

    pla

--- DoanVB/DoanVB/DanhMuc/frmThemNhanVien.vb
+++ DoanVB/DoanVB/DanhMuc/frmThemNhanVien.vb
@@ -3,8 +3,15 @@ Imports DTO
 Public Class frmThemNhanVien
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLuu.Click
-        Dim nv As New NhanVienDTO(Me.txtMa.Text)
-
-        NhanVienBUS.ThemNhanVien()
+        Dim nv As New NhanVienDTO(Me.txtMa.Text, Me.txtTen.Text, Me.txtChucVu.Text, _
+                                  Me.txtDiaChi.Text, Me.txtEmail.Text, Me.txtDienThoai.Text, _
+                                  Me.cboBoPhan.SelectedValue.ToString(), Me.cboNguoiQuanLi.SelectedValue.ToString(), _
+                                  Me.cktinhTrang.Checked)
+        Dim kq As Boolean = NhanVienBUS.ThemNhanVien(nv)
+        If kq = True Then
+            MessageBox.Show("Thêm thành công")
+        Else
+            MessageBox.Show("Không thêm được")
+        End If
     End Sub
 End Class
